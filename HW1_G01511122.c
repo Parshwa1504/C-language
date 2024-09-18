4e5231e1e3badc5bcee748ec95af39b9b13814bc
Parshwa1504 <parshwagandhi0403@gmail.com>
2024-09-18 11:10:20 -0400

    Update HW1_G01511122.c

--- HW1_G01511122.c
+++ HW1_G01511122.c
@@ -1,3 +1,19 @@
+/*
+Design and implement a C language program based on the following specifications:
+
+1. The user interface will prompt for ten unique character strings to be entered from the keyboard. Each character string must have a
+strlen() value between 1 and 25 inclusive, otherwise an error message is displayed and the user is re-prompted. The program sorts
+this series of ten character strings (based on strcmp()), and reprints all ten strings in ascending OR descending order based on user
+specification. The program then prints and labels the character string with the lowest ascii value and that with the highest ascii value
+as determined by strcmp(). Character strings may NOT include the following characters: ’+’, ’*’, ’#’, ’$’, ‘%’, ‘^’, ’(’, or ’)’. (6 Points)
+
+2. Include additional error checking for: Empty string, Duplicate string, Character other than ‘A’ or ‘D’ for sort specification. (2 Point)
+
+3. Include at least 4 user defined functions outside of the main(). (2 point)
+
+4. One global variable may be used. Each additional global variable will result in a one point penalty.
+*/    
+
 // Name : Parshwa Divyang Gandhi
 // G-Number : G01511122
 
